0e1ce6058c9d69e87f677c68f81db5ba50e5e502
kdehghan <kdehghan@68962db9-4a4b-4818-9c81-d2bbc50cf17f>
2014-09-08 23:16:24 +0000

    t4 is also done, BOOYAAA!

---
+++ 1/t4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void)
+{
+   float num=0;
+   int num_floor=0, num_round=0, num_ceil=0;
+   //Fetch the number
+   int error=scanf("%f",&num);
+	if (error == 0)
+          printf("scanf error: (%d)\n",error);
+        else
+         {   
+             //Calculating the floor, rounf and ceil
+             num_floor=floor(num);
+             num_round=round(num);
+             num_ceil=ceil(num);
+             //Printing the results
+             printf(  "%d %d %d\n",num_floor,num_round,num_ceil );
+         }
+   return 0;
+}
