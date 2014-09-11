d68ebe3c2a87e10f1ce7a5ba3f3716d95585cc37
kdehghan <kdehghan@68962db9-4a4b-4818-9c81-d2bbc50cf17f>
2014-09-11 02:04:17 +0000

    t5 updatedm

--- 1/t5.c
+++ 1/t5.c
@@ -3,18 +3,18 @@
 
 int main(void)
 {
-    int end=0;
     float num=0;
     int num_floor=0, num_round=0, num_ceil=0;
-    do
-    {
-        scanf("%f",&num);
+    int result=0;
+    while((result=scanf("%f",&num))!=EOF)
+     {
+        
         num_floor=floor(num);
         num_round=round(num);
         num_ceil=ceil(num);
         printf(  "\n%d %d %d\n",num_floor,num_round,num_ceil );
         
-    }while((end=getchar())!=EOF);
+     }
     printf("Done.\n");
     
 }
