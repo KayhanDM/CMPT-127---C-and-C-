9b8bbf21be289e62c954ff51871676998c5c276b
kdehghan <kdehghan@68962db9-4a4b-4818-9c81-d2bbc50cf17f>
2014-09-15 18:32:03 +0000

    task1 submit

---
+++ 2/funcs.h
@@ -0,0 +1,19 @@
+// returns the larger of the two arguments
+int maximum( int x, int y)
+{
+  if( x > y ) 
+  {
+    return x;
+  } 
+  return y;
+}
+
+// returns the smaller of the two arguments
+int minimum( int x, int y)
+{
+  if( x < y ) 
+  {
+    return x;
+  } 
+  return y;
+}
